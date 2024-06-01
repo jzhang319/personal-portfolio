eaa074a8792fd732b47b7c825595b4b597a57f76
jzhang319 <jzhang319@gmail.com>
2024-06-01 03:41:18 -0400

    updated logo

--- src/components/utils/Logo.jsx
+++ src/components/utils/Logo.jsx
@@ -13,7 +13,7 @@ const Logo = ({ url = "/", text = false }) => {
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               className="h-14 max-h-full w-auto"
-              src="/images/logo.png"
+              src="/images/logo5.png"
               alt="Zhang"
             />
           </>
